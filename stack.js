5af84e89b5655198a976ee11f8e222ff2d427c0f
Miguel Montalvo <miguel.montalvo@archeytpethemes.co>
2023-06-30 19:34:41 -0700

    ♻️ Object-based stack

--- stack.js
+++ stack.js
@@ -1,35 +1,48 @@
 export default class Stack {
-  /** @type {number[]} */
+  /** @type {{ [key: number]: number }} */
   items
 
   constructor() {
-    this.items = []
+    this.count = 0
+    this.items = {}
   }
 
   /**
    * @param {number} element
    */
   push(element) {
-    this.items.push(element)
+    this.items[this.count] = element
+    this.count++
   }
 
   size() {
-    return this.items.length
+    return this.count
   }
 
   peek() {
-    return this.items[this.items.length - 1]
+    if (this.isEmpty()) {
+      return undefined
+    }
+    return this.items[this.count - 1]
   }
 
   isEmpty() {
-    return this.items.length === 0
+    return this.count === 0
   }
 
   clear() {
-    this.items = []
+    this.items = {}
+    this.count = 0
   }
 
   pop() {
-    this.items.pop()
+    if (this.isEmpty()) {
+      return undefined
+    }
+
+    this.count--
+    const result = this.items[this.count]
+    delete this.items[this.count]
+    return result
   }
 }
