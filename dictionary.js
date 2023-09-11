7a599d22dd64201c0f52c47b721a7bed7845a332
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-09-10 22:11:01 -0400

    🚧 Dictionary

---
+++ dictionary.js
@@ -0,0 +1,31 @@
+import { defaultToString } from './utils.js'
+import ValuePair from './value-pair.js'
+
+/**
+ * @template T, U
+ */
+export default class Dictionary {
+  constructor(toStrFn = defaultToString) {
+    /** @private */
+    this.toStrFn = toStrFn
+    /**
+     * @type {Object.<string, ValuePair.<T, U>>}
+     * @private
+     */
+    this.table = {}
+  }
+
+  /**
+   * @param {T} key
+   * @param {U} value
+   */
+  set(key, value) {
+    if (key === null || value === null) {
+      return false
+    }
+    const tableKey = this.toStrFn(key)
+    const valuePair = new ValuePair(key, value)
+    this.table[tableKey] = valuePair
+    return true
+  }
+}
---
+++ value-pair.js
@@ -0,0 +1,17 @@
+/**
+ * @template T, U
+ */
+export default class ValuePair {
+  /**
+   * @param {T} key
+   * @param {U} value
+   */
+  constructor(key, value) {
+    this.key = key
+    this.value = value
+  }
+
+  toString() {
+    return `[${this.key}: ${this.value}]`
+  }
+}
