7237e0e7fcc8a763f1f84b87339611fe2c105968
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-10-12 18:07:53 -0400

    ✨ Height of a BST
    (Maximum depth)

---
+++ avl-tree.js
@@ -0,0 +1,34 @@
+import BST from './binary-search-tree-iterative.js'
+
+export default class AVLTree extends BST {
+  /**
+   * @param {AVLTree | null} node
+   */
+  getNodeHeight(node = this) {
+    if (!node) {
+      return 0
+    }
+
+    /** @type {BST[]} */
+    const queue = []
+    queue.push(node)
+
+    let levelCount = 0
+
+    while (queue.length) {
+      levelCount++
+
+      for (let i = 0; i < queue.length; i++) {
+        const treeNode = queue.shift()
+
+        if (treeNode?.left) {
+          queue.push(treeNode?.left)
+        }
+        if (treeNode?.right) {
+          queue.push(treeNode?.right)
+        }
+      }
+    }
+    return levelCount - 1
+  }
+}
