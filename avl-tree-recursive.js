6fd60f009115f8703029a436d07f53964a767295
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-10-13 14:17:34 -0400

    ✨ Maximum depth rcursive

---
+++ avl-tree-recursive.js
@@ -0,0 +1,31 @@
+import BinarySearchTree from './binary-search-tree.js'
+import { TreeNode as Node } from './node.js'
+
+/**
+ * @template {number} T
+ * @extends BinarySearchTree<T>
+ */
+export default class AVLTree extends BinarySearchTree {
+  /**
+   * @param {Node<T> | null} node
+   * @description
+   * Average: O(log n) time | O(log n) space
+   * Worst: O(n) time | O(n) space
+   */
+  getNodeHeight(node) {
+    if (!node) {
+      return 0
+    }
+    let leftCount = 0
+    let rightCount = 0
+    if (node.left) {
+      leftCount++
+      leftCount += this.getNodeHeight(node.left)
+    }
+    if (node.right) {
+      rightCount++
+      rightCount += this.getNodeHeight(node.right)
+    }
+    return Math.max(leftCount, rightCount)
+  }
+}
---
+++ test/avl-tree-recursive.js
@@ -0,0 +1,13 @@
+import test from 'ava'
+import AVLTree from '../avl-tree-recursive.js'
+
+test('maximum depth', (t) => {
+  const bst = new AVLTree()
+  bst.insert(3)
+  bst.insert(2)
+  bst.insert(6)
+  bst.insert(5)
+  bst.insert(4)
+  bst.insert(7)
+  t.is(bst.getNodeHeight(bst.root), 3)
+})
