f877c09e62850a9ff9317290b48e98b32ba73c3a
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-11-13 18:05:52 -0500

    🚧 Graph

---
+++ graph.js
@@ -0,0 +1,57 @@
+import Dictionary from './dictionary.js'
+
+export default class Graph {
+  /** @private */
+  isDirected
+  /**
+   * @private
+   * @type {Array<string | number>}
+   */
+  vertices
+  /**
+   * @private
+   * @type {Dictionary<string | number, (string | number)[]>}
+   */
+  adjList
+
+  constructor(isDirected = false) {
+    this.isDirected = isDirected
+    this.vertices = []
+    this.adjList = new Dictionary()
+  }
+
+  /**
+   * @param {string | number} v
+   */
+  addVertex(v) {
+    if (!this.vertices.includes(v)) {
+      this.vertices.push(v)
+      this.adjList.set(v, [])
+    }
+  }
+
+  /**
+   * @param {string | number} v
+   * @param {string | number} w
+   */
+  addEdge(v, w) {
+    if (!this.adjList.get(v)) {
+      this.addVertex(v)
+    }
+    if (!this.adjList.get(w)) {
+      this.addVertex(w)
+    }
+    this.adjList.get(v)?.push(w)
+    if (!this.isDirected) {
+      this.adjList.get(w)?.push(v)
+    }
+  }
+
+  getVertices() {
+    return this.vertices
+  }
+
+  getAdjList() {
+    return this.adjList
+  }
+}
