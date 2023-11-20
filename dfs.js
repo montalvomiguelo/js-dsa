95fcbabc0557dcf106c8e79c8cba32da17f99a22
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-11-20 18:21:29 -0500

    ✨ DFS

---
+++ dfs.js
@@ -0,0 +1,31 @@
+/**  @typedef {import('./dictionary').default<string|number, (string|number)[]>} Dictionary */
+
+/**
+ * @param {import('./graph').Graph} graph
+ * @param {(v: string | number) => void} callback
+ */
+export default function dfs(graph, callback) {
+  const vertices = graph.getVertices()
+  const adjList = graph.getAdjList()
+  const seen = new Set()
+  const vertex = vertices[0]
+  helper(vertex, callback, adjList, seen)
+}
+
+/**
+ * @param {number | string} vertex
+ * @param {(v: string | number) => void} callback
+ * @param {Dictionary} adjList
+ * @param {Set<string|number>} seen
+ */
+function helper(vertex, callback, adjList, seen) {
+  callback(vertex)
+  seen.add(vertex)
+  const adjVertices = adjList.get(vertex)
+  if (!adjVertices) return
+  for (let i = 0; i < adjVertices.length; i++) {
+    const adjVertex = adjVertices[i]
+    if (seen.has(adjVertex)) continue
+    helper(adjVertex, callback, adjList, seen)
+  }
+}
