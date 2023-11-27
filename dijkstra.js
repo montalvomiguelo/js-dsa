35608826b21bfb48681372ec829508c486ba9757
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-11-27 17:25:00 -0500

    ✨ Dijkstra algorithm

--- dijkstra.js
+++ dijkstra.js
@@ -2,31 +2,28 @@
  * @param {Array<Array<number>>} graph
  * @param {number} src
  * @description
- * O(v^2) time | O(v) space
+ * O(v^2 + e) time | O(v) space
  */
 export default function dijkstra(graph, src) {
   /** @type {Array<number>} */
   const distances = []
-  /** @type {Array<boolean>} */
-  const visited = []
+  /** @type {Set<number>} */
+  const visited = new Set()
 
   for (let i = 0; i < graph.length; i++) {
     distances[i] = Infinity
-    visited[i] = false
   }
 
   distances[src] = 0
 
-  for (let i = 0; i < graph.length; i++) {
+  while (visited.size < graph.length) {
     const u = minDistance(distances, visited)
-    visited[u] = true
+    visited.add(u)
+
     for (let v = 0; v < graph.length; v++) {
-      if (
-        !visited[v] &&
-        graph[u][v] !== 0 &&
-        distances[u] !== Infinity &&
-        distances[u] + graph[u][v] < distances[v]
-      ) {
+      if (visited.has(v)) continue
+
+      if (graph[u][v] !== 0 && distances[u] + graph[u][v] < distances[v]) {
         distances[v] = distances[u] + graph[u][v]
       }
     }
@@ -37,18 +34,20 @@ export default function dijkstra(graph, src) {
 
 /**
  * @param {Array<number>} distances
- * @param {Array<boolean>} visited
+ * @param {Set<number>} visited
  */
 function minDistance(distances, visited) {
   let min = Infinity
-  let minIndex = -1
+  let minIdx = -1
 
   for (let v = 0; v < distances.length; v++) {
-    if (!visited[v] && distances[v] <= min) {
+    if (visited.has(v)) continue
+
+    if (distances[v] <= min) {
       min = distances[v]
-      minIndex = v
+      minIdx = v
     }
   }
 
-  return minIndex
+  return minIdx
 }
