55c3c8607b9f5cf6d335e71f0e74e330a0265746
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-13 16:56:32 -0600

    ✨ Bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,17 @@
+import { swap } from './utils.js'
+
+/**
+ * @param {number[]} array
+ * @description
+ * O(n^2) time | O(1) space
+ */
+export default function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1)
+      }
+    }
+  }
+  return array
+}
