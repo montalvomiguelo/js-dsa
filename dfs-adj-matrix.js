17649a6fb29b15dd6c442a722645c9d49fa11bb3
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-14 17:57:17 -0600

    ✨ DFS Adjacency matrix

---
+++ dfs-adj-matrix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} adjMatrix
+ * @param {(v: number) => void} callback
+ */
+export default function dfs(adjMatrix, callback) {
+  const seen = new Set()
+  const vertex = 0
+  dfsVisit(vertex, callback, seen, adjMatrix)
+}
+
+/**
+ * @param {number} vertex
+ * @param {(v: number) => void} callback
+ * @param {Set<number>} seen
+ * @param {number[][]} adjMatrix
+ */
+function dfsVisit(vertex, callback, seen, adjMatrix) {
+  seen.add(vertex)
+  callback(vertex)
+  const adjVertices = adjMatrix[vertex]
+  for (let i = 0; i < adjVertices.length; i++) {
+    const adjVertex = i
+    if (adjVertices[adjVertex] === 0) continue
+    if (seen.has(adjVertex)) continue
+    dfsVisit(adjVertex, callback, seen, adjMatrix)
+  }
+}
---
+++ test/dfs-adj-matrix.js
@@ -0,0 +1,15 @@
+import test from 'ava'
+import dfs from '../dfs-adj-matrix.js'
+
+test('visit all vertices', (t) => {
+  const adjMatrix = [
+    [0, 1, 1, 1],
+    [1, 0, 1, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0],
+  ]
+  /** @type {number[]} */
+  const vertices = []
+  dfs(adjMatrix, (v) => vertices.push(v))
+  t.deepEqual(vertices, [0, 1, 2, 3])
+})
