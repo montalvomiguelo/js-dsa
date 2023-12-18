aef83cc0d75dd3ba56477629b348de819121cc8b
Miguel Montalvo <miguel.montalvo@archetypethemes.co>
2023-12-17 20:00:14 -0600

    ✨ Selection sort

---
+++ selection-sort.js
@@ -0,0 +1,22 @@
+import { swap } from './utils.js'
+
+/**
+ * @param {number[]} array
+ * @description
+ * O(n^2) time | O(1) space
+ */
+export default function selectionSort(array) {
+  let indexMin
+  for (let i = 0; i < array.length - 1; i++) {
+    indexMin = i
+    for (let j = i; j < array.length; j++) {
+      if (array[indexMin] > array[j]) {
+        indexMin = j
+      }
+    }
+    if (i !== indexMin) {
+      swap(array, i, indexMin)
+    }
+  }
+  return array
+}
